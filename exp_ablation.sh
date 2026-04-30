2c3271b37f604ea48bc5fd2ead2919de54f2d1bd
xiulinyang <xiulin.yang.compling@gmail.com>
2026-04-30 07:05:43 -0400

    update ablation code

--- exp_ablation.sh
+++ exp_ablation.sh
@@ -2,21 +2,22 @@
 
 
 MODEL_NAME=$1
-EVAL_LANG=$2
+LANGS=('ZH' 'AR' 'RU' 'EN')
 
-PPL_TYPES=( 'bpb' )
+PPL_TYPES=( 'cpb' 'sent-nll' 'token-nll' 'ppl')
 EVAL_DATA=( 'pud' 'flores')
 
 
+for EVAL_LANG in "${LANGS[@]}"; do
+  for ppl_type in "${PPL_TYPES[@]}"; do
+    for eval_data in "${EVAL_DATA[@]}"; do
 
-for ppl_type in "${PPL_TYPES[@]}"; do
-  for eval_data in "${EVAL_DATA[@]}"; do
+      echo "Running: MODEL_NAME=$MODEL_NAME, EVAL_LANG=$EVAL_LANG, PPL_TYPE=$ppl_type, EVAL_DATA=$eval_data"
 
-    echo "Running: MODEL_NAME=$MODEL_NAME, EVAL_LANG=$EVAL_LANG, PPL_TYPE=$ppl_type, EVAL_DATA=$eval_data"
+      python ppl_minicons_model_specific.py "$MODEL_NAME" "$EVAL_LANG" "$ppl_type" "$eval_data"
 
-    python ppl_minicons_model_specific.py "$MODEL_NAME" "$EVAL_LANG" "$ppl_type" "$eval_data"
-
-    echo "---------------------------------------------"
-#          y | rm -rf ~/.cache/huggingface/*
+      echo "---------------------------------------------"
+  #          y | rm -rf ~/.cache/huggingface/*
+    done
   done
 done
