88f77e75586e0492c180eb51cd2bfd5b5088f3eb
xiulinyang <xiulin.yang.compling@gmail.com>
2026-04-30 10:16:52 -0400

    count ctc

---
+++ data_stats_model_specific.py
@@ -0,0 +1,46 @@
+import os
+from transformers import AutoTokenizer
+from pathlib import Path
+import argparse
+from tqdm import tqdm
+
+def count_tokens(sent, tokenizer):
+    tokens = tokenizer.encode(sent, add_special_tokens=False)
+    return len(tokens)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Count tokens, characters, and bits in sentences.')
+    parser.add_argument('model', type=str, help='the name of the language model')
+    parser.add_argument('language', type=str,  help='the name of the language')
+    parser.add_argument('data', type=str, help='the data name')
+    args = parser.parse_args()
+    language = args.language
+    model = args.model
+    data = args.data
+
+
+
+
+    if data == 'flores':
+        file = Path(f'data/flores/{language}.txt').read_text().strip().split('\n')
+    elif data == 'parallel10':
+        file = Path(f'data/parallel10/{language}/test/{language}.txt').read_text().strip().split('\n')
+    elif data == 'pud':
+        file = Path(f'data/pud/{language}.txt').read_text().strip().split('\n')
+    elif data == 'parallel3':
+        file = Path(f'data/parallel3/{language}/test/{language}.txt').read_text().strip().split('\n')
+    else:
+        raise ValueError(f"Unsupported data: {data}")
+
+
+
+
+    tokenizer = AutoTokenizer.from_pretrained(f'{model}')
+    model_name = model.split('/')[-1]
+    os.makedirs(f'language_stats/{data}', exist_ok=True)
+    with open(f'language_stats/{data}/{language}_{model_name}_token.tsv', 'w') as f:
+        f.write('Sentence\tTokens\n')
+        for sent in tqdm(file):
+            token_count = count_tokens(sent, tokenizer)
+            f.write(f'{sent}\t{token_count}\n')
--- lang_stats_script.sh
+++ lang_stats_script.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-LANGS=('EN' 'ZH' 'AR')
-DATA=('parallel3' 'pud' 'flores')
+MODEL=$1
+LANGS=('EN' 'ZH' 'AR' 'RU')
+DATA=('pud' 'flores')
 for LANG in "${LANGS[@]}"; do
   for DATASET in "${DATA[@]}"; do
     echo "Running: LANG=$LANG, DATASET=$DATASET"
     #python data_stats.py "$LANG" "$DATASET"
-    python ctc.py "$LANG" "$DATASET"
+    python data_stats_model_specific.py "$MODEL" "$LANG" "$DATASET"
     echo "Finished: $LANG $DATASET"
     echo "---------------------------------------------"
   done
