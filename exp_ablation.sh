e627bfcedb33a7549b245dc153e856ff91e9e062
xiulinyang <xiulin.yang.compling@gmail.com>
2026-04-29 20:35:45 -0400

    update ablation code

---
+++ exp_ablation.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+
+MODEL_NAME=$1
+EVAL_LANG=$2
+
+PPL_TYPES=('ppl' 'token-nll' 'bpb' 'sent-nll' 'cpb' 'mrr')
+EVAL_DATA=('parallel3' 'pud' 'flores')
+
+
+
+for ppl_type in "${PPL_TYPES[@]}"; do
+    for eval_data in "${EVAL_DATA[@]}"; do
+
+    echo "Running: MODEL_NAME=$MODEL_NAME, EVAL_LANG=$EVAL_LANG, PPL_TYPE=$ppl_type, EVAL_DATA=$eval_data"
+
+    python ppl_minicons_model_specific.py "$MODEL_NAME" "$EVAL_LANG" "$ppl_type" "$eval_data"
+
+    echo "---------------------------------------------"
+#          y | rm -rf ~/.cache/huggingface/*
+  done
+done
---
+++ ppl_minicons_model_specific.py
@@ -0,0 +1,141 @@
+import math
+import sys
+from minicons import scorer
+
+from ppl_minicons import ppl_type
+
+sys.path.append("..")
+from pathlib import Path
+from transformers import GPT2LMHeadModel,AutoTokenizer
+from tqdm import tqdm
+import pandas as pd
+import argparse
+import os
+import numpy as np
+import torch
+
+CHECKPOINTS = [10]
+DATA_DIR = 'data/'
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument('model_name', type=str, help="model names")
+parser.add_argument('eval_language', type=str, help="model names")
+parser.add_argument('ppl_type', help='Type of perplexity')
+parser.add_argument('eval_data', help='Evaluation data')
+# Get args
+args = parser.parse_args()
+model_name = args.model_name
+language = args.eval_language
+ppl_type = args.ppl_type
+# Get path to model
+model_path = f"{model_name}"
+tokenizer = AutoTokenizer.from_pretrained(f'{model_path}', use_fast=True)
+eval_data = args.eval_data
+
+if eval_data =='en':
+    test_file='wmt19-ende-original.en'
+elif eval_data=='de':
+    test_file='wmt19-ende-original.de'
+elif eval_data=='de-ar':
+    test_file ='wmt19-ende-ar.de'
+elif eval_data=='de-arp':
+    test_file ='wmt19-ende-arp.de'
+elif eval_data =='de-p':
+    test_file = 'wmt19-ende-p.de'
+elif eval_data =='flores':
+    test_file = f'data/flores/{language}.txt'
+elif eval_data =='parallel10':
+    test_file =f'data/parallel10/{language}/test/{language}.txt'
+elif eval_data =='parallel3':
+    test_file =f'data/parallel3/{language}/test/{language}.txt'
+elif eval_data =='pud':
+    test_file = f'data/pud/{language}.txt'
+else:
+    raise ValueError(f"Unsupported eval_data: {eval_data}")
+
+test_texts = Path(test_file).read_text().strip().split('\n')
+BATCH_SIZE = 8
+
+ppl_df = pd.DataFrame({
+    "Sentences": test_texts,
+    # "Original": file_text_sequences, # to debug
+})
+
+def compute_sentence_mrr(model, tokenizer, sent_batch):
+    mrrs = []
+
+    for sent in sent_batch:
+        input_ids = tokenizer(sent, return_tensors="pt").input_ids[0]
+        if len(input_ids) < 2:
+            mrrs.append(None)
+            continue
+        inputs = input_ids[:-1].unsqueeze(0)
+        labels = input_ids[1:]
+
+        with torch.no_grad():
+            logits = model(inputs).logits[0]
+
+        preds_logits = logits.cpu().numpy()
+        labels_np = labels.cpu().numpy().reshape(-1, 1)
+
+        gold_logits = np.take_along_axis(preds_logits, labels_np, axis=1)
+        higher_counts = np.sum(preds_logits > gold_logits, axis=1)
+        ranks = higher_counts + 1
+
+        sent_mrr = np.mean(1.0 / ranks)
+        mrrs.append(sent_mrr)
+    return mrrs
+
+
+device = "cpu"
+for j, ckpt in enumerate(CHECKPOINTS):
+    print(f"Epoch:{ckpt}")
+    ilm_model = scorer.IncrementalLMScorer(f'{model_path}', device, revision=f'checkpoint-{ckpt}')
+    model = GPT2LMHeadModel.from_pretrained(f'{model_path}', revision=f'checkpoint-{ckpt}').to(device)
+    tokenizer = AutoTokenizer.from_pretrained(f'{model_path}', use_fast=True)
+    vocab_size = model_name.split('_')[4]
+    metrics = []
+    failed_batch=0
+    for i in tqdm(range(0, len(test_texts), BATCH_SIZE)):
+        batch_text = test_texts[i:i+BATCH_SIZE]
+        batch_text = [tokenizer.decode(tokenizer.encode(x, truncation=True, max_length=120, add_special_tokens=False)) for x in batch_text]
+
+        if ppl_type=='sent-nll':
+            nll = ilm_model.sequence_score(batch_text, reduction=lambda x: -x.sum(0).item())
+        elif ppl_type=='token-nll':
+            nll = ilm_model.sequence_score(batch_text, reduction=lambda x: -x.mean(0).item())
+        elif ppl_type=='ppl':
+            nll = ilm_model.sequence_score(batch_text, reduction=lambda x: -x.mean(0).item())
+            nll = [math.exp(x) for x in nll]
+        elif ppl_type =='bpb':
+            nll_sum = ilm_model.sequence_score(batch_text, reduction=lambda x: -x.sum(0).item())
+            bytes_seq = [tokenizer.decode(tokenizer.encode(x, truncation=True, max_length=120, add_special_tokens=False)).encode('utf-8') for x in batch_text]
+            assert len(nll_sum)==len(bytes_seq)
+            nll = [(x/len(y))/math.log(2) for x,y in zip(nll_sum, bytes_seq)]
+        elif ppl_type =='cpb':
+            nll_sum = ilm_model.sequence_score(batch_text, reduction=lambda x: -x.sum(0).item())
+            chars_seq = [
+                len(tokenizer.decode(tokenizer.encode(x, truncation=True, max_length=120, add_special_tokens=False))) for x in batch_text]
+            nll = [(x / y)/math.log(2) for x, y in zip(nll_sum, chars_seq)]
+        elif ppl_type=='mrr':
+            nll=compute_sentence_mrr(model, tokenizer,batch_text)
+        else:
+            raise ValueError(f"Unsupported ppl_type: {ppl_type}")
+        metrics.extend(nll)
+
+
+    ppl_df[f"Epoch-{ckpt}"] = metrics
+
+model_sub_name = model_name.split('/')[-1]
+# Write results to CSV
+directory = f"results_{ppl_type}_{eval_data}"
+os.makedirs(directory, exist_ok=True)
+file = directory + \
+       f"/{model_sub_name}_{language}.csv"
+
+if not os.path.exists(directory):
+    os.makedirs(directory)
+
+print(f"Writing results to CSV: {file}")
+ppl_df.to_csv(file)
